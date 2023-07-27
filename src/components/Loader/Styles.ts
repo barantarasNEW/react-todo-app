5ae2c8b440590d6ed1b9d544410c1a01cb4a126f
Taras <baran.taras.dev@gmail.com>
2023-07-28 01:50:32 +0200

    created loader

---
+++ src/components/Loader/Styles.ts
@@ -0,0 +1,9 @@
+import {styled} from "@mui/material/styles";
+import {Box} from "@mui/material";
+
+export const StyledBox = styled(Box)({
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)'
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
+import {Suspense} from "react";
 import ReactDOM from 'react-dom/client';
 import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
-import App from './App';
 import {PersistGate} from 'redux-persist/integration/react';
 import {persistStore} from "redux-persist";
-import {Suspense} from "react";
+import App from './App';
+import Loader from "./components/Loader/Loader";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,7 +17,7 @@ const persist = persistStore(store);
 root.render(
   <HashRouter>
     <Provider store={store}>
-      <Suspense fallback={<p>Loading...</p>}>
+      <Suspense fallback={<Loader />}>
         <PersistGate persistor={persist}>
           <App />
         </PersistGate>
